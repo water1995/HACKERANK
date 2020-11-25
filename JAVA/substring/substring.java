598577009d843573cac9f031fd55c7df377066f8
water1995 <genevievexal@hotmail.com>
2020-11-25 22:28:51 +0530

    25/11/2020

---
+++ JAVA/substring/substring.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+
+public class substring{
+
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+String s = sc.next();
+int start = sc.nextInt();
+int end = sc.nextInt();
+System.out.println("Genevieve Xalxo");
+System.out.println(s.substring(start,end));
+}}
