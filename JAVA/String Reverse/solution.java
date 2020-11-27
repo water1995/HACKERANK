4f885a6e78c14dc5e555fd01c2eaf961aec3cf9b
water1995 <genevievexal@hotmail.com>
2020-11-27 21:40:32 +0530

    27/11/2020

--- JAVA/String Reverse/solution.java
+++ JAVA/String Reverse/solution.java
@@ -6,8 +6,9 @@ public class solution{
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        
+
         String A=sc.next();
+        
         /* Enter your code here. Print output to STDOUT. */
         StringBuffer b = new StringBuffer(A);
         b.reverse();
