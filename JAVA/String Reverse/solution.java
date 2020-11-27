323ff3844ed0ce08e7a7851096fba1c83ec66266
water1995 <genevievexal@hotmail.com>
2020-11-27 21:35:45 +0530

    27/11/2020

---
+++ JAVA/String Reverse/solution.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class solution{
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        StringBuffer b = new StringBuffer(A);
+        b.reverse();
+        String c = b.toString();
+        if(A.equals(c)){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+        
+    }
+}
